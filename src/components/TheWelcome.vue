a150a92c453d236d701baa776ab38d14e6516a9c
Mugheessaad <kambohsaad@gmail.com>
2024-02-27 11:12:30 +0500

    changed by Mughees

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -16,7 +16,7 @@ import SupportIcon from "./icons/IconSupport.vue";
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener"
-      >official documentation</a
+      >Changed by mughees</a
     >
     provides you with all information you need to get started.
   </WelcomeItem>
